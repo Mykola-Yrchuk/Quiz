b38dd1925e9ced66853ae2177e7cf35a40dc33ea
xTentomushii <luffemafia@gmail.com>
2026-05-28 14:16:30 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 from quiz import is_correct
 
-def test_correct_answer():
-    assert is_correct("8", "8") == True
+
+def test_is_correct():
+    assert is_correct(5, 5) == True
 
 def test_wrong_answer():
     assert is_correct("7", "8") == False
