d3bee882a1bf641fd87724590f2adc15e957efe2
xTentomushii <luffemafia@gmail.com>
2026-05-28 14:28:27 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from quiz import is_correct
 
 
 def test_is_correct():
     assert is_correct(5, 5) == True
+
 
 def test_wrong_answer():
     assert is_correct("7", "8") == False
